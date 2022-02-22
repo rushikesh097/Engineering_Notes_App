e627e3b5d2c3683f58ef687991016834a8005fe5
Rushikesh Gaikwad <grushikesh907@gmail.com>
2022-02-22 18:23:41 +0530

    Tab Fragments Added : Rushikesh

--- app/src/main/java/com/example/engineeringnotes/HomeFragment.java
+++ app/src/main/java/com/example/engineeringnotes/HomeFragment.java
@@ -1,20 +1,49 @@
 package com.example.engineeringnotes;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.engineeringnotes.adapters.VPFragmentAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class HomeFragment extends Fragment {
     //22-02-22
     //hbdc
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    VPFragmentAdapter vpFragmentAdapter;
+    private Context context;
+    FragmentActivity fragmentActivity;
+    private String[] tabNames = new String[]{"SEM I","SEM II"};
+
+    public HomeFragment(Context context, FragmentActivity fragmentActivity){
+        this.context = context;
+        this.fragmentActivity = fragmentActivity;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        viewPager2 = view.findViewById(R.id.viewPager2);
+        tabLayout = view.findViewById(R.id.tabLayout);
+        vpFragmentAdapter = new VPFragmentAdapter(fragmentActivity);
+        vpFragmentAdapter.setContext(context);
+        viewPager2.setAdapter(vpFragmentAdapter);
+
+        new TabLayoutMediator(tabLayout,viewPager2,(((tab, position) -> tab.setText(tabNames[position]))))
+                .attach();
+
+        return view;
     }
 }
--- app/src/main/java/com/example/engineeringnotes/StartFragment.java
+++ app/src/main/java/com/example/engineeringnotes/StartFragment.java
@@ -1,16 +1,27 @@
 package com.example.engineeringnotes;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class StartFragment extends Fragment {
+
+    private Context context;
+    private FragmentActivity fragmentActivity;
+
+    public StartFragment(Context context, FragmentActivity fragmentActivity) {
+        this.context = context;
+        this.fragmentActivity = fragmentActivity;
+    }
 
     RadioButton btn1,btn2,btn3,btn4;
     @Override
@@ -22,14 +33,16 @@ public class StartFragment extends Fragment {
         btn2 = view.findViewById(R.id.radioButton2);
         btn3 = view.findViewById(R.id.radioButton3);
         btn4 = view.findViewById(R.id.radioButton4);
-
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Zal !", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Zal", Toast.LENGTH_SHORT).show();
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,new HomeFragment(context,fragmentActivity))
+                        .commit();
             }
         });
-
         return view;
     }
 }
