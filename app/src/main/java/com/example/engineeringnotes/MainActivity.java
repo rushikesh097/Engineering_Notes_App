febd1cc6d4ec90df71e041f21aa7426c258d5bf4
Rushikesh Gaikwad <grushikesh907@gmail.com>
2022-02-23 18:40:23 +0530

    Toolbar class removed : Rushikesh

--- app/src/main/java/com/example/engineeringnotes/MainActivity.java
+++ app/src/main/java/com/example/engineeringnotes/MainActivity.java
@@ -23,7 +23,6 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
-    Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
@@ -41,9 +40,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ActionBar Bar = getSupportActionBar();
-//        Bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         FrameLayout frameLayout = findViewById(R.id.frame_layout);
         getSupportFragmentManager()
                 .beginTransaction()
@@ -51,11 +47,12 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
 
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.NavigationView);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.menu_Open,R.string.menu_close);
         actionBarDrawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -89,7 +86,5 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-        getSupportActionBar().hide();
     }
 }
