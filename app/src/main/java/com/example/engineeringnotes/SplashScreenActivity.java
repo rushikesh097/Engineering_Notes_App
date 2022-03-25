9b4682e2738a52b8c5108285ad2547e8a721240c
Rushikesh Gaikwad <grushikesh907@gmail.com>
2022-03-25 15:02:54 +0530

    Splash Screen Added : Rushikesh

--- app/src/main/java/com/example/engineeringnotes/SplashScreenActivity.java
+++ app/src/main/java/com/example/engineeringnotes/SplashScreenActivity.java
@@ -2,10 +2,12 @@ package com.example.engineeringnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
